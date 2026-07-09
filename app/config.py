52eccfa64af9ea6c1c82f4e779aa239ea10cc890
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-07-09 13:07:15 -0300

    fix: Settings not finding .env

--- app/config.py
+++ app/config.py
@@ -1,14 +1,11 @@
-from pydantic_settings import BaseSettings
-from urllib.parse import urlsplit, urlunsplit
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
+ROOT = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     database_url: str
-    model_config = {"env_file": ".env"}
 
-    @staticmethod
-    def get_admin_url(database_url: str) -> str:
-        r = urlsplit(database_url)
-        r = r._replace(path='/postgres')
-
-        return urlunsplit(r)    
+    model_config = SettingsConfigDict(
+        env_file=ROOT / ".env",
+    )
