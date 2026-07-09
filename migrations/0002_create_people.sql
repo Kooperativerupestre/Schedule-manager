eba3d5d637d547a6ac3df74bee5c5f01cfc123bd
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-07-09 18:20:39 -0300

    refactor: add unique constraint on phone_number and updated_at column

--- migrations/0002_create_people.sql
+++ migrations/0002_create_people.sql
@@ -1,7 +1,8 @@
 CREATE TABLE people (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
-    phone_number TEXT NOT NULL,
+    phone_number TEXT NOT NULL UNIQUE,
     created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    last_update TIMESTAMPTZ DEFAULT now() NOT NULL,
     status BOOLEAN NOT NULL DEFAULT TRUE
 );
