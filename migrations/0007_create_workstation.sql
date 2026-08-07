fd95d3f22e5e1b97e125a1422c32fa397de72037
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-07 20:05:31 -0300

    fix: workstation migration

--- migrations/0007_create_workstation.sql
+++ migrations/0007_create_workstation.sql
@@ -1,76 +1,117 @@
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+
+
 CREATE TABLE workstations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     unit_id UUID REFERENCES units(id) ON DELETE CASCADE,
-    created_at TIMESTAMPTZ DEFAULT now() NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     name TEXT NOT NULL,
     description TEXT
 );
 
+
 CREATE TABLE workstation_holidays (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    workstation_id UUID REFERENCES workstations(id) ON DELETE CASCADE NOT NULL,
+    workstation_id UUID NOT NULL
+        REFERENCES workstations(id) ON DELETE CASCADE,
+
     name TEXT NOT NULL,
     description TEXT,
     holiday_range TSTZRANGE NOT NULL,
 
     CONSTRAINT no_overlapping_workstation_holidays
-    EXCLUDE USING gist (
-    (workstation_id) WITH =,
-    (holiday_range) WITH &&
-    ),
-    CHECK (NOT isempty(holiday_range))
+        EXCLUDE USING gist (
+            workstation_id WITH =,
+            holiday_range WITH &&
+        ),
+
+    CONSTRAINT valid_workstation_holiday_range
+        CHECK (NOT isempty(holiday_range))
 );
+
 
 CREATE TABLE workstation_exceptions (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    workstation_id UUID REFERENCES workstation(id) ON DELETE CASCADE,
+    workstation_id UUID NOT NULL
+        REFERENCES workstations(id) ON DELETE CASCADE,
+
     status TEXT NOT NULL,
-    CHECK (status IN 'available', 'unavailable'),
     exception_range TSTZRANGE NOT NULL,
-    description TEXT DEFAULT NULL,
+    description TEXT,
 
-    CONSTRAINT no_overlapping_exceptions 
-    EXCLUDE USING gist (
-        (workstation_id) WITH =,
-        (exception_range) WITH &&
-    ),
+    CONSTRAINT valid_workstation_exception_status
+        CHECK (status IN ('available', 'unavailable')),
 
-    CHECK (NOT isempty(exception_range))
+    CONSTRAINT no_overlapping_exceptions
+        EXCLUDE USING gist (
+            workstation_id WITH =,
+            exception_range WITH &&
+        ),
+
+    CONSTRAINT valid_workstation_exception_range
+        CHECK (NOT isempty(exception_range))
 );
+
 
 CREATE TABLE workstation_common_hours (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    workstation_id UUID REFERENCES workstation(id) ON DELETE CASCADE,
+    workstation_id UUID NOT NULL
+        REFERENCES workstations(id) ON DELETE CASCADE,
+
     status TEXT NOT NULL,
-    CHECK (status IN ('available', 'unavailable')),
     hours_range TSTZRANGE NOT NULL,
 
-    CONSTRAINT no_overlapping_hours
-    EXCLUDE USING gist (
-        (workstation_id) WITH =,
-        (hours_range) WITH &&
-    ),
+    CONSTRAINT valid_workstation_common_hours_status
+        CHECK (status IN ('available', 'unavailable')),
 
-    CHECK (NOT isempty(hours_range))
+    CONSTRAINT no_overlapping_hours
+        EXCLUDE USING gist (
+            workstation_id WITH =,
+            hours_range WITH &&
+        ),
+
+    CONSTRAINT valid_workstation_common_hours_range
+        CHECK (NOT isempty(hours_range))
 );
+
 
 CREATE TABLE schedules (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    workstation_id UUID REFERENCES workstations(id) ON DELETE CASCADE NOT NULL,
-    person_id UUID REFERENCES people(id) ON DELETE CASCADE NOT NULL,
+
+    workstation_id UUID NOT NULL
+        REFERENCES workstations(id) ON DELETE CASCADE,
+
+    person_id UUID NOT NULL
+        REFERENCES people(id) ON DELETE CASCADE,
+
     schedule_range TSTZRANGE NOT NULL,
+
     status TEXT NOT NULL DEFAULT 'scheduled',
-    CHECK (status IN ('scheduled', 'cancelled', 'completed')),
+
+    active_schedule_range TSTZRANGE
+        GENERATED ALWAYS AS (
+            CASE
+                WHEN status IN ('scheduled', 'completed')
+                    THEN schedule_range
+                ELSE NULL
+            END
+        ) STORED,
+
+    CONSTRAINT valid_schedule_status
+        CHECK (status IN ('scheduled', 'cancelled', 'completed')),
+
+    CONSTRAINT valid_schedule_range
+        CHECK (NOT isempty(schedule_range)),
 
     CONSTRAINT no_overlapping_schedules
-    EXCLUDE USING gist (
-        (workstation_id) with =,
-        (schedule_range) WITH &&
-    ) WHERE status = IN ('scheduled', 'completed'),
-    CONSTRAINT no_overlapping_same_person_schedules 
-    EXCLUDE USING gist (
-        (person_id) WITH =,
-        (schedule_range) WITH &&
-    ) WHERE status = ('scheduled', 'completed'),
-    CHECK (NOT isempty(schedule_range))
+        EXCLUDE USING gist (
+            workstation_id WITH =,
+            active_schedule_range WITH &&
+        ),
+
+    CONSTRAINT no_overlapping_same_person_schedules
+        EXCLUDE USING gist (
+            person_id WITH =,
+            active_schedule_range WITH &&
+        )
 );
